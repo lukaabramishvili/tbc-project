b059d652685a76351f76e1444c11f923ab462ca8
lukaabramishvili <92173962+lukaabramishvili@users.noreply.github.com>
2024-10-13 23:49:48 +0400

    add home page

--- src/app/home/page.jsx
+++ src/app/home/page.jsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import { useState } from 'react';
+import './index.css';
+
+const slides = [
+  'Slide 1 Content',
+  'Slide 2 Content',
+  'Slide 3 Content'
+];
 
 export default function Home() {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  const nextSlide = () => {
+    setCurrentSlide((prev) => (prev + 1) % slides.length);
+  };
+
+  const prevSlide = () => {
+    setCurrentSlide((prev) => (prev - 1 + slides.length) % slides.length);
+  };
+
   return (
-    <div>Home</div>
-  )
+    <main className='homeContainer'>
+      <h2>Welcome to Our Website!</h2>
+      <div className='homeSlider'>
+        <div className='homeSlide'>
+          {slides[currentSlide]}
+        </div>
+        <button className='prev' onClick={prevSlide}>
+          &#10094;
+        </button>
+        <button className='next' onClick={nextSlide}>
+          &#10095;
+        </button>
+      </div>
+    </main>
+  );
 }
