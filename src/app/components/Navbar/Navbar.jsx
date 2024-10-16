f569a54b48fe65a75529d62827bc05e64582ed68
lukaabramishvili <92173962+lukaabramishvili@users.noreply.github.com>
2024-10-16 21:25:25 +0400

    change navbar names

--- src/app/components/Navbar/Navbar.jsx
+++ src/app/components/Navbar/Navbar.jsx
@@ -6,10 +6,10 @@ export default function Navbar() {
     <nav>
       <ul>
         <li><Link href="/">Home</Link></li>
-        <li><Link href="/shop">Shop</Link></li>
+        <li><Link href="/products">Products</Link></li>
         <li><Link href="/contact">Contact</Link></li>
         <li><Link href="/about">About</Link></li>
-        <li><Link href="/blog">Blog</Link></li>
+        <li><Link href="/posts">Posts</Link></li>
       </ul>
     </nav>
   );
