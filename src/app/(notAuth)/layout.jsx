794202a4a0110896f3af5d388d8906a89e2191b4
lukaabramishvili <92173962+lukaabramishvili@users.noreply.github.com>
2024-12-14 17:54:58 +0400

    fix(login): fix login problem

---
+++ src/app/(notAuth)/layout.jsx
@@ -0,0 +1,29 @@
+'use client';
+import '../../globals.css';
+import { useRouter } from 'next/navigation';
+
+export default function NotAuthLayout({ children }) {
+  const router = useRouter();
+
+  return (
+    <html lang="en">
+      <head>
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap"
+          rel="stylesheet"
+        />
+      </head>
+      <body>
+        <div id="root">
+          <div className="application">{children}</div>
+        </div>
+      </body>
+    </html>
+  );
+}
