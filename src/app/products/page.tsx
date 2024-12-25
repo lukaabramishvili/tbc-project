b2d7867660c6107a5deb260d260ddc46eb9a72b4
lukaabramishvili <92173962+lukaabramishvili@users.noreply.github.com>
2024-12-25 20:29:45 +0400

    fix(product style): change product page styles and add darkmode

--- src/app/products/page.tsx
+++ src/app/products/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 import AddProductDialog from "../components/AddProductDialog/AddProductDialog";
 import { useCart } from "../components/providers/CartProvider";
-import "./index.css";
 import { useEffect, useState } from "react";
 
 export interface Product {
@@ -39,32 +38,36 @@ function Products() {
   }, [isRetriggered]);
 
   return (
-    <div className="product-page container min-h-96">
-      <h1 className="">Our Products</h1>
-      <div className="flex flex-row items-center justify-center p-2 mt-0 pt-0">
+    <div className="container min-h-screen p-6 mx-auto bg-gray-100 dark:bg-gray-700">
+      <h1 className="text-center text-4xl font-bold mb-6 dark:text-white">Our Products</h1>
+      <div className="flex justify-center mb-4">
         <AddProductDialog retriggerFetch={retriggerFetch}></AddProductDialog>
       </div>
 
-      <div className="product-grid">
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
         {products.map((item) => (
           <div
             key={item.id}
-            className="w-60 h-72 flex flex-col items-center text-start p-5 border rounded-2xl bg-slate-200"
-          >
-            <div className="font-bold w-full p-2">Name: {item.title}</div>
-            <img className="w-56 h-56 items-center" src={item.img_url}></img>
-            <div className="flex flex-row items-center justify-center ">
-              <div className="font-bold w-full p-2 ">
-                Price: {item.price / 100}$
+            className="cursor-pointer flex flex-col items-center p-5 border rounded-xl bg-white dark:bg-gray-800 shadow-xl hover:shadow-2xl dark:shadow-xl dark:hover:shadow-2xl transform hover:scale-105 dark:hover:scale-105 transition-all duration-300 ease-in-out"
+            >
+            <div className="font-semibold text-lg text-gray-900 dark:text-gray-100 mb-2">{item.title}</div>
+            <img
+              className="w-full h-56 object-cover rounded-lg mb-4"
+              src={item.img_url}
+              alt={item.title}
+            />
+            <div className="flex justify-between w-full items-center mt-auto">
+              <div className="font-semibold text-gray-900 dark:text-gray-100">
+                ${ (item.price / 100).toFixed(2) }
               </div>
-              <div
-                className="flex text-sm w-40 underline"
+              <button
+                className="cursor-pointer border-none px-6 py-2 text-sm font-semibold text-white bg-blue-600 rounded-full shadow-md hover:bg-blue-700 hover:shadow-lg focus:outline-none focus:ring-2 focus:ring-blue-500 dark:bg-blue-500 dark:hover:bg-blue-400 dark:focus:ring-blue-300 transition-all duration-300"
                 onClick={() => {
                   addItemToCart({ product: item, quantity: 1 });
                 }}
               >
                 Add to cart
-              </div>
+              </button>
             </div>
           </div>
         ))}
