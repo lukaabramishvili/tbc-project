934e92c2fea5556ab56d962e0827ce27733cb3bb
Levani Sarishvili <levanisarishvili322@gmail.com>
2024-12-26 12:46:34 +0400

    feat: create orders page

--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -1,3 +1,4 @@
+import Link from "next/link";
 import { createClient } from "../../utils/supabase/server";
 import { logout } from "../logout/actions";
 import Image from "next/image";
@@ -5,8 +6,12 @@ import Image from "next/image";
 export default async function Profile() {
   const supabase = await createClient();
   const { data, error } = await supabase.auth.getUser();
+
+  if (error) {
+    console.error(error);
+    return;
+  }
   const user = data?.user;
-  console.log(user?.email);
 
   return (
     <main className="flex flex-col gap-8 min-h-full items-center justify-center text-center max-w-[136rem] mx-auto p-8">
@@ -21,15 +26,23 @@ export default async function Profile() {
         <a href={`mailto:${user?.email}`}>{user?.email}</a>
       </div>
 
-      {/* Logout */}
-      <form action={logout}>
-        <button
-          type="submit"
-          className="rounded-lg px-6 py-2 cursor-pointer text-white bg-slate-700 hover:bg-slate-600 duration-300"
-        >
-          Logout
-        </button>
-      </form>
+      <div className="flex gap-4 justify-center">
+        {/* Navigate to orders */}
+        <Link href="/orders">
+          <button className="rounded-lg px-6 py-2 cursor-pointer text-white bg-slate-700 hover:bg-slate-600 duration-300">
+            Orders
+          </button>
+        </Link>
+        {/* Logout */}
+        <form action={logout}>
+          <button
+            type="submit"
+            className="rounded-lg px-6 py-2 cursor-pointer text-white bg-slate-700 hover:bg-slate-600 duration-300"
+          >
+            Logout
+          </button>
+        </form>
+      </div>
     </main>
   );
 }
