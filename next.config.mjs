2f9e1b32712a6e1198ba1904ab8ee16deaf9b108
lukaabramishvili <92173962+lukaabramishvili@users.noreply.github.com>
2025-01-17 21:04:07 +0400

    fix(next.config): fix next config for vercel

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: "./dist", // Changes the build output directory to `./dist/`.
-
   images: {
     remotePatterns: [
       {
