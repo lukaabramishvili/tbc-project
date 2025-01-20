6679ff9ce7c57f8d44aaff8f02fa9012504ea4a1
lukaabramishvili <92173962+lukaabramishvili@users.noreply.github.com>
2025-01-20 19:41:50 +0400

    fix(style): fix responsive for Browse by Categories

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "363c2584b992a074"; }
+/******/ 		__webpack_require__.h = function() { return "63568a7c6f10f767"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
